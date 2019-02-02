0f52b19b123b0a8a63ec043a3f26379a7a6c9a10
Holkan <luah613@gmail.com>
2019-02-01 18:40:05 -0600

    conditionally renderin the fkn form

--- class_5/lab/src/components/ContentList.js
+++ class_5/lab/src/components/ContentList.js
@@ -4,12 +4,18 @@ import TaskList from './TaskList';
 export default class ContentList extends Component{
 	
 	render(){
-		const {date, methods, tasks} = this.props
+		const {date, methods, tasks, newTask} = this.props
+		let hideShow;
+		if(newTask){
+			hideShow = 'show_form';
+		}else{
+			hideShow = 'hide_form';
+		}
+
 		return(
 			<Fragment>
 				<div className="main_content sides">
-
-					<form onSubmit={methods.formSubmit}>
+					<form onSubmit={methods.formSubmit} className={hideShow}>
 						<input name="mynewtask" type="text" /> 
 						<button type="submit" onClick={methods.btnClick}>Save</button>
 					</form>
